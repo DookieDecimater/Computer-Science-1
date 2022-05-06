a09a5c326f434cfc7cb9e86d74fd5fa98bf7b77c
DookieDecimater <98102655+DookieDecimater@users.noreply.github.com>
2022-05-06 14:35:24 -0400

    Add files via upload

---
+++ sort_a_list.py
@@ -0,0 +1,13 @@
+#sort a list
+#jackson bauer
+list = [1,99,5,32,8,59,22,22,43,7,6,11]
+input = 'do you want to sort by (asc) or (desc)'
+def lnum(lnum):
+    if input.lower == asc:
+        list.sort(reverse=True)
+        print(list)
+    elif input.lower == desc:
+        lsit.sort(reverse=False)
+        print(list)
+
+lnum(list)
