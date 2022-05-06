8a5797fcd631547071c68dd3f706a4e3b4c3809d
DookieDecimater <98102655+DookieDecimater@users.noreply.github.com>
2022-05-06 15:17:15 -0400

    Add files via upload

--- TTK.py
+++ TTK.py
@@ -1,9 +1,12 @@
-import time
-board = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
+#jackson bauer
+#tik tac toe
+
+import time#imports time library
+board = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}#dictonary
 
 count = 0       
 winner = False      
-play = True     
+play = True#defines play     
 tie = False     
 curr_player = ''    
 player_details = [] 
@@ -11,14 +14,14 @@ player_details = []
 
 def get_player_details(curr_player):
     if curr_player == 'A':
-        return ['B','O']
+        return ['B','O']#stores player value
     else:
         return ['A','X']
     
 
 def print_board():
    
-    for i in board:
+    for i in board:#for loop
         print( i, ':', board[i], ' ', end='')
         if i%3 == 0:
             print()
@@ -34,7 +37,7 @@ def win_game(marker, player_id):
     board[3] == marker and board[6] == marker and board[9] == marker or \
     board[1] == marker and board[5] == marker and board[9] == marker or \
     board[3] == marker and board[5] == marker and board[7] == marker:
-
+#win condition if statements
         print_board()
         time.sleep(1)
         print('congrats player', player_id, 'you win')
@@ -67,7 +70,7 @@ while play:
     print_board()
 
     player_details = get_player_details(curr_player)
-    curr_player = player_details[0]
+    curr_player = player_details[0]#player A
     print("Player {} where do you want to go?".format(curr_player))
     input_slot = int(input())
 
